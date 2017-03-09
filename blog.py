81395606f8bf6698c9401f42f617908892d77dd0
Nick Garanko <ngaranko@gmail.com>
2017-03-09 17:00:12 +0100

    New blog

--- blog.py
+++ blog.py
@@ -1,40 +1,48 @@
 import logging
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import yaml
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
+CONFIG_FILE = 'blog.yml'
+
 
 class Blog(object):
-    def __init__(self):
+    def __init__(self, config):
         self.env = Environment(
             loader=FileSystemLoader('./src'),
             autoescape=select_autoescape(['html'])
         )
+        self.config = config
 
-    def render(self, source, destination, context):
+    def render(self, source, destination, context=None):
         """
         Simply glue files together.
         """
+        full_context = config.get('context', {})
+
+        if context is not None:
+            full_context.update(context)
+
         logger.info("Rendering file: {} > {}".format(source, destination))
         logger.debug(" Context: {}".format(repr(context)))
         template = self.env.get_template(source)
-        template.stream(**context).dump(destination)
+        template.stream(**full_context).dump(destination)
 
-    def render_all(self, config):
-        for file_config in config['files']:
+    def render_all(self):
+        for file_config in self.config['files']:
             self.render(
                 source=file_config['source'],
                 destination=file_config['destination'],
-                context={}
+                context=file_config.get('context', None)
             )
 
 
 if __name__ == '__main__':
-    blog = Blog()
-    blog.render_all(dict(
-        files=[
-            dict(source='index.html',
-                 destination='index.html')
-        ]
-    ))
+    with open(CONFIG_FILE, 'r') as config_file:
+        config = yaml.load(config_file)
+
+    logger.debug("Config: {}".format(repr(config)))
+    blog = Blog(config)
+    blog.render_all()
